9a96e4b29360581d8ac06e27bd58e2ca44336a0b
Mateus Donaire <canalmateusdonaire@gmail.com>
2021-06-17 01:41:34 -0300

    Introducao ao DotNet 5

---
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProEventos.API.Model;
+
+namespace ProEventos.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {
+
+        public IEnumerable<Evento> _evento = new Evento[] {
+            new Evento(){
+                EventoId = 1,
+                Local = "Belo Horizonte",
+                Tema = "Angula 11 e .Net 5",
+                Lote = "1º Lote",
+                QtdPessoas = 250,
+                DataEvento = DateTime.Now.AddDays(2).ToString("dd/mm/yyyy"),
+                ImagemURL = "Imagem1.png"
+            },
+            new Evento(){
+                EventoId = 2,
+                Local = "São Paulo",
+                Tema = "Angula e suas novidades!",
+                Lote = "2º Lote",
+                QtdPessoas = 350,
+                DataEvento = DateTime.Now.AddDays(3).ToString("dd/mm/yyyy"),
+                ImagemURL = "Imagem2.png"
+            }
+        };
+
+        public EventoController()
+        {
+
+        }
+
+        [HttpGet]
+        public IEnumerable<Evento> Get()
+        {
+            return _evento;
+        }
+
+        [HttpPost]
+        public string Post()
+        {
+            return "Exemplo de POST";
+        }
+
+        [HttpPut("{id}")]
+        public string Put(int id)
+        {
+            return $"Exemplo de Put com id: {id}";
+        }
+
+        [HttpDelete("{id}")]
+        public string Delete(int id)
+        {
+            return $"Exemplo de Delete com id: {id}";
+        }
+    }
+}
